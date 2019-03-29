7597bb059262e91dc5079ad927955cdba2d9d862
Thais Moraes <murici.thais@gmail.com>
2019-03-29 11:45:51 -0300

    Adding Mapbox map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import ReactMapGL from 'react-map-gl';
 
 class App extends Component {
+  state = {
+    viewport: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      latitude: -23.5439948,
+      longitude: -46.6065452,
+      zoom: 14,
+    },
+  };
+
+  componentDidMount() {
+    window.addEventListener('resize', this.resize);
+    this.resize();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.resize);
+  }
+
+  resize = () => {
+    const { viewport } = this.state;
+    this.setState({
+      viewport: {
+        ...viewport,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      },
+    });
+  };
+
   render() {
+    const { viewport } = this.state;
     return (
-      <div className="App">
-        <h1>Hello World!</h1>
-      </div>
+      <ReactMapGL
+        {...viewport}
+        onViewportChange={newViewport => this.setState({ viewport: newViewport })}
+      />
     );
   }
 }
